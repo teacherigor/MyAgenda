eae4fad2ee6951ffc2ac67df40b555a9d9e1114c
teacherigor <teacherigor6@gmail.com>
2017-09-03 17:23:51 +0300

    click on a close button to hide the current list item

--- MyAgenda.js
+++ MyAgenda.js
@@ -8,3 +8,13 @@ for (i = 0; i < myNodelist.length; i++) {
 	span.appendChild(txt);
 	myNodelist[i].appendChild(span);
 }
+
+// Click on a close button to hide the current list item
+var close = document.getElementByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+	close[i].onclick = function() {
+		var div = this.parentElement;
+		div.style.display = "none";
+	}
+}
