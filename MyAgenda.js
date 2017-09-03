163ae7b919255dadf90e9ecba14c0e9f8d176069
teacherigor <teacherigor6@gmail.com>
2017-09-03 17:08:19 +0300

    create a close button and append it to each item_2

--- MyAgenda.js
+++ MyAgenda.js
@@ -4,4 +4,7 @@ var i;
 for (i = 0; i < myNodelist.length; i++) {
 	var span = document.createElement("SPAN");
 	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	myNodelist[i].appendChild(span);
 }
